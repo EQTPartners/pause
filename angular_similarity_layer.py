a2a0490abcec654b6f24bc4dac2e91f636c9b4da
cao-lele <lele.cao@eqtpartners.com>
2021-09-03 17:51:16 +0200

    fix comments for data_utils

--- angular_similarity_layer.py
+++ angular_similarity_layer.py
@@ -10,10 +10,22 @@ import tensorflow as tf
 
 
 class AngularSimilarity(tf.keras.layers.Layer):
+    """A custom layer class that performs angular similarity calculation."""
+
     def __init__(self):
+        """Initialize an AngularSimilarity layer"""
         super(AngularSimilarity, self).__init__()
 
-    def call(self, x1, x2):
+    def call(self, x1: tf.Tensor, x2: tf.Tensor) -> tf.Tensor:
+        """[summary]
+
+        Args:
+            x1 (tf.Tensor): The first input
+            x2 (tf.Tensor): The second input
+
+        Returns:
+            tf.Tensor: the angular similarity between x1 and x2
+        """
         cosine_sim = -tf.keras.losses.cosine_similarity(x1, x2)
         cosine_sim = tf.clip_by_value(cosine_sim, -1.0, 1.0)
         cosine_sim = tf.expand_dims(cosine_sim, axis=-1)
