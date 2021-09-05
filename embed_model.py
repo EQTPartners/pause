320c5888a0476f9f1f1bd19338fe979bcb88f2e0
cao-lele <lele.cao@eqtpartners.com>
2021-09-05 15:50:35 +0200

    refine comments for embed_model.py

--- embed_model.py
+++ embed_model.py
@@ -6,6 +6,7 @@ License: MIT, https://github.com/EQTPartners/pause/LICENSE.md
 
 
 import os
+from typing import Any
 import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text
@@ -14,11 +15,19 @@ _FEATURE_KEY = "sentence"
 
 
 class EmbedModel(tf.keras.Model):
+    """The implementation of the sentence embedding model"""
+
     def __init__(
         self,
-        bert_model_link,
-        max_seq_len=128,
-    ):
+        bert_model_link: str,
+        max_seq_len: int = 128,
+    ) -> None:
+        """Initializer of the embedding model
+
+        Args:
+            bert_model_link (str): The TF-hub link to the pre-trained BERT model.
+            max_seq_len (int, optional): The maximum sequence length. Defaults to 128.
+        """
         super(EmbedModel, self).__init__()
         print("bert_model_link=", bert_model_link)
         self.bert_layer = hub.KerasLayer(bert_model_link, trainable=True)
@@ -33,7 +42,15 @@ class EmbedModel(tf.keras.Model):
             token_out_type=tf.int32,
         )
 
-    def tokenize_single_sentence_unpad(self, sequence: tf.Tensor):
+    def tokenize_single_sentence_unpad(self, sequence: tf.Tensor) -> tf.Tensor:
+        """Tokenize one input sentence.
+
+        Args:
+            sequence (tf.Tensor): The input sentence.
+
+        Returns:
+            tf.Tensor: The tokenized sentence.
+        """
         word_ids = self.tokenizer.tokenize(sequence)
         word_ids = word_ids.merge_dims(-2, -1)
         cls_token = tf.fill([word_ids.nrows(), 1], self._cls_id)
@@ -45,12 +62,19 @@ class EmbedModel(tf.keras.Model):
 
         return word_ids
 
-    def tokenize_single_sentence_pad(self, sequence: tf.Tensor):
-        word_ids = self.tokenize_single_sentence_unpad(
-            tf.reshape(sequence, [-1])
-        )
+    def tokenize_single_sentence_pad(self, sequence: tf.Tensor) -> dict:
+        """Tokenize one input sentence and pad it to the maximun length allowed.
+
+        Args:
+            sequence (tf.Tensor): The input sentence.
+
+        Returns:
+            dict: A dict that contains word IDs, input mask, and segment IDs.
+        """
+        word_ids = self.tokenize_single_sentence_unpad(tf.reshape(sequence, [-1]))
         word_ids = word_ids.to_tensor(
-            shape=[None, self.max_seq_len], default_value=self._pad_id,
+            shape=[None, self.max_seq_len],
+            default_value=self._pad_id,
         )
         input_mask = tf.cast(tf.not_equal(word_ids, self._pad_id), tf.int32)
         segment_ids = tf.zeros_like(word_ids, tf.int32)
@@ -60,23 +84,30 @@ class EmbedModel(tf.keras.Model):
             "input_type_ids": segment_ids,
         }
 
-    def call(self, data):
+    def call(self, data: tf.Tensor) -> tf.Tensor:
+        """Perform the actual embedding operation.
+
+        Args:
+            data (tf.Tensor): The input sentence.
+
+        Returns:
+            tf.Tensor: The embedding of the input sentence.
+        """
         bert_input = self.tokenize_single_sentence_pad(data)
         bert_out = self.bert_layer(bert_input)
         bert_out_selected = tf.math.reduce_mean(bert_out["sequence_output"], axis=1)
         return bert_out_selected
 
-    def save_model(self, filepath):
+    def save_model(self, filepath: str) -> None:
+        """Save the embedding model.
+
+        Args:
+            filepath (str): The folder to which the model will be saved.
+        """
         serving_model_split = filepath.split("/")
-        model_dir = "/".join(
-            serving_model_split[: len(serving_model_split) - 1]
-        )
-        embed_model_dir = os.path.join(
-            model_dir, "embed", serving_model_split[-1]
-        )
-        tf.get_logger().info(
-            "Saving embed model to {}".format(embed_model_dir)
-        )
+        model_dir = "/".join(serving_model_split[: len(serving_model_split) - 1])
+        embed_model_dir = os.path.join(model_dir, "embed", serving_model_split[-1])
+        tf.get_logger().info("Saving embed model to {}".format(embed_model_dir))
         signatures = {
             "serving_default": self._get_serve_tf_examples_fn().get_concrete_function(
                 tf.TensorSpec(shape=[None], dtype=tf.string, name="examples")
@@ -90,18 +121,14 @@ class EmbedModel(tf.keras.Model):
             signatures=signatures,
         )
 
-    def _get_serve_tf_examples_fn(self):
+    def _get_serve_tf_examples_fn(self) -> tf.function:
         """Returns a function that parses a serialized tf.Example."""
 
         @tf.function
-        def serve_tf_examples_fn(serialized_tf_examples):
+        def serve_tf_examples_fn(serialized_tf_examples: tf.Tensor) -> dict:
             """Returns the output to be used in the serving signature."""
-            feature_spec = {
-                _FEATURE_KEY: tf.io.FixedLenFeature([], tf.string)
-            }
-            parsed_features = tf.io.parse_example(
-                serialized_tf_examples, feature_spec
-            )
+            feature_spec = {_FEATURE_KEY: tf.io.FixedLenFeature([], tf.string)}
+            parsed_features = tf.io.parse_example(serialized_tf_examples, feature_spec)
             output = self(parsed_features[_FEATURE_KEY])
             return {"output": output}
 
